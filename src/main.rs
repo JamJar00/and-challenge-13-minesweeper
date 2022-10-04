1e30c9656c119955dd034165fcc6316c015c4fe4
Jamie Read <jamie.read@and.digital>
2022-10-04 12:09:40 +0100

    Add comments

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ fn main() {
 pub fn minesweeper(grid: &Vec<char>) -> Vec<char> {
     let mut solved: Vec<char> = vec![];
 
-
+    // Iterate over all tiles and compile into output, any - characters need solving
     for (pos, tile) in grid.iter().enumerate() {
         let solved_tile = match tile {
             'X' => 'X',
@@ -20,6 +20,7 @@ pub fn minesweeper(grid: &Vec<char>) -> Vec<char> {
 }
 
 fn calculate_mine_count(grid: &Vec<char>, pos: usize) -> usize {
+    // Check all neighbouring tiles for mines
     let n = try_get_tile(grid, pos, 0, -1) == 'X';
     let ne = try_get_tile(grid, pos, 1, -1) == 'X';
     let e = try_get_tile(grid, pos,  1, 0) == 'X';
@@ -29,19 +30,24 @@ fn calculate_mine_count(grid: &Vec<char>, pos: usize) -> usize {
     let w = try_get_tile(grid, pos, -1, 0) == 'X';
     let nw = try_get_tile(grid, pos, -1, -1) == 'X';
 
-
+    // Count how many of the neighbouring tiles were actually mines
     return [n, ne, e, se, s, sw, w, nw].into_iter().filter(|b| *b).count();
 }
 
 fn try_get_tile(grid: &Vec<char>, pos: usize, offset_x: i8, offset_y: i8) -> char {
+    // Provides a safe way of accessing a tile at a given position in relation to another
+
+    // Work out the x/y coordinate of wherever we're trying to access
     let width = f64::sqrt(grid.len() as f64) as usize;
     let x = (pos % width) as isize + offset_x as isize;
     let y = (pos / width) as isize + offset_y as isize;
 
+    // Check the x/y is in bounds of the board
     if x < 0 || x as usize >= width || y < 0 || y as usize >= width {
         return '-';
     }
 
+    // Return the content
     return grid[y as usize * width + x as usize];
 }
 
