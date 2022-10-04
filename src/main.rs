df2d1d693fc074d04437c798aeba2526af2835e1
Jamie Read <jamie.read@and.digital>
2022-10-04 12:19:27 +0100

    Add additional tests

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,11 @@ fn main() {
 }
 
 pub fn minesweeper(grid: &Vec<char>) -> Vec<char> {
+    // Don't support nonsquare boards because we won't be able to work out the width
+    if (f64::sqrt(grid.len() as f64) as usize).pow(2) != grid.len() {
+        panic!("Cannot operate on nonsquare grids")
+    }
+
     let mut solved: Vec<char> = vec![];
 
     // Iterate over all tiles and compile into output, any - characters need solving
@@ -58,7 +63,7 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_add() {
+    fn test_valid_board_1() {
         let input: Vec<char> = vec![
             '-', '-', '-', '-', '-',
             '-', '-', '-', '-', '-',
@@ -75,5 +80,117 @@ mod tests {
             '2', '2', '1', '1', '1',
             '0', '0', '0', '1', 'X'
         ]);
+    }
+
+    #[test]
+    fn test_valid_board_2() {
+        let input: Vec<char> = vec![
+            '-', 'X', '-', '-', '-',
+            '-', '-', 'X', '-', '-',
+            'X', 'X', '-', '-', '-',
+            '-', '-', 'X', '-', '-',
+            '-', '-', 'X', '-', 'X'
+        ];
+
+        assert_eq!(minesweeper(&input),
+        [
+            '1', 'X', '2', '1', '0',
+            '3', '4', 'X', '1', '0',
+            'X', 'X', '3', '2', '0',
+            '2', '4', 'X', '3', '1',
+            '0', '2', 'X', '3', 'X'
+        ]);
+    }
+
+    #[test]
+    fn test_valid_board_3() {
+        let input: Vec<char> = vec![
+            '-', '-', '-', '-', '-',
+            '-', '-', '-', '-', '-',
+            '-', '-', '-', '-', '-',
+            '-', '-', '-', '-', '-',
+            '-', '-', '-', '-', '-'
+        ];
+
+        assert_eq!(minesweeper(&input),
+        [
+            '0', '0', '0', '0', '0',
+            '0', '0', '0', '0', '0',
+            '0', '0', '0', '0', '0',
+            '0', '0', '0', '0', '0',
+            '0', '0', '0', '0', '0'
+        ]);
+    }
+
+    #[test]
+    fn test_valid_board_4() {
+        let input: Vec<char> = vec![
+            'X', 'X', 'X', 'X', 'X',
+            'X', 'X', 'X', 'X', 'X',
+            'X', 'X', 'X', 'X', 'X',
+            'X', 'X', 'X', 'X', 'X',
+            'X', 'X', 'X', 'X', 'X'
+        ];
+
+        assert_eq!(minesweeper(&input),
+        [
+            'X', 'X', 'X', 'X', 'X',
+            'X', 'X', 'X', 'X', 'X',
+            'X', 'X', 'X', 'X', 'X',
+            'X', 'X', 'X', 'X', 'X',
+            'X', 'X', 'X', 'X', 'X'
+        ]);
+    }
+
+    #[test]
+    fn test_valid_board_5() {
+        let input: Vec<char> = vec![
+            '-', 'X', '-', '-', 'X', 'X', 'X',
+            '-', '-', 'X', '-', 'X', '-', 'X',
+            'X', 'X', '-', '-', 'X', 'X', 'X',
+            '-', '-', 'X', '-', '-', '-', '-',
+            '-', '-', 'X', '-', 'X', '-', '-',
+            '-', 'X', '-', '-', '-', 'X', '-',
+            '-', '-', '-', '-', 'X', '-', '-'
+        ];
+
+        assert_eq!(minesweeper(&input),
+        [
+            '1', 'X', '2', '3', 'X', 'X', 'X',
+            '3', '4', 'X', '4', 'X', '8', 'X',
+            'X', 'X', '3', '4', 'X', 'X', 'X',
+            '2', '4', 'X', '4', '3', '4', '2',
+            '1', '3', 'X', '3', 'X', '2', '1',
+            '1', 'X', '2', '3', '3', 'X', '1',
+            '1', '1', '1', '1', 'X', '2', '1'
+        ]);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_board_with_invalid_characters() {
+        let input: Vec<char> = vec![
+            '-', 'X', '-', '-', '-',
+            '-', '-', 'X', '-', '-',
+            'X', '~', '-', '-', '-',
+            '-', '-', 'X', '-', '-',
+            '-', '-', 'X', '-', 'X'
+        ];
+
+        minesweeper(&input);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_board_with_nonsquare_dimensions() {
+        let input: Vec<char> = vec![
+            '-', 'X', '-', '-',
+            '-', '-', 'X', '-',
+            'X', 'X', '-', '-',
+            '-', '-', 'X', '-',
+            '-', '-', 'X', '-',
+        ];
+
+        minesweeper(&input);
     }
 }
